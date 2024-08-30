18cb26c5fa88617d6a41c07eaf016f910027b3d8
Fulvia Mourikis <zum281@gmail.com>
2024-08-30 19:16:09 +0200

    feat: create calculate function

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-alert(1);
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const parseInput = (input) => {
+  const inputArray = input.split(" ");
+  const operatorsList = ["+", "-", "*", "/"];
+  const numbers = inputArray
+    .filter((char) => !operatorsList.includes(char))
+    .map((x) => parseFloat(x));
+  const operators = inputArray.filter((char) => operatorsList.includes(char));
+  return { numbers, operators };
+};
+
+const calculate = (numbers, operators) => {
+  console.log(numbers);
+  console.log(operators);
+  let result = numbers[0];
+  for (let i = 0; i < operators.length; i++) {
+    switch (operators[i]) {
+      case "+":
+        result = sum(result, numbers[i + 1]);
+        break;
+      case "-":
+        result = subtract(result, numbers[i + 1]);
+        break;
+      case "*":
+        result = multiply(result, numbers[i + 1]);
+        break;
+      case "/":
+        result = divide(result, numbers[i + 1]);
+        break;
+    }
+  }
+  return result;
+};
